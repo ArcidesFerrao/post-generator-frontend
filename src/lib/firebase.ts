c00ecba0fb8ef8ee2a0e5d5659655c629789c045
ArcidesFerrao <cidesferrao@gmail.com>
2025-07-18 22:55:22 +0200

    secrets hidden

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import {getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCtCKabJMaGVdx_v3lPwmKxEdKvATjnCm0",
-  authDomain: "quote-1fdd1.firebaseapp.com",
-  projectId: "quote-1fdd1",
-  storageBucket: "quote-1fdd1.firebasestorage.app",
-  messagingSenderId: "771845341508",
-  appId: "1:771845341508:web:ce86bc838b915ca9789711",
-  measurementId: "G-KT4D13NLYZ"
+    apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
 }
 
 const app = initializeApp(firebaseConfig);
